91e062833d020dd3363326321a1d408cae910bb5
Valentin <47915711+alkoria@users.noreply.github.com>
2026-07-15 00:55:03 +0300

    Menu

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,4 +1,42 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+
+const createNavBlock = () => {
+  return [
+    { text: "Домашняя", link: "/" },
+    { text: "Навигация", link: "/menu" },
+  ];
+};
+
+const createStandardBlock = (title, extraItems = []) => {
+  return {
+    text: title,
+    items: [
+      { text: "Главная", link: "/" },
+      { text: "Навигация", link: "/menu" },
+      ...extraItems, // Добавляем уникальные пункты, если есть
+    ],
+  };
+};
+const createLinuxBlock = (title, extraItems = []) => {
+  return {
+    text: title,
+    items: [
+      { text: "Linux", link: "/linux" },
+      { text: "Навигация Linux", link: "#" },
+      ...extraItems, // Добавляем уникальные пункты, если есть
+    ],
+  };
+};
+const createWindowsBlock = (title, extraItems = []) => {
+  return {
+    text: title,
+    items: [
+      { text: "Windows", link: "#" },
+      { text: "Навигация Windows", link: "#" },
+      ...extraItems, // Добавляем уникальные пункты, если есть
+    ],
+  };
+};
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -7,40 +45,45 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      ...createNavBlock(),
+      createLinuxBlock("Linux"),
+      createWindowsBlock("Windows"),
+      // { text: 'Домашняя', link: '/' },
+      // { text: 'Навигация', link: '/menu' }
     ],
 
-    sidebar: [
-      {
-        text: 'Примеры',
-        items: [
-          { text: 'Примеры разметки', link: '/markdown-examples' },
-          { text: 'Примеры API', link: '/api-examples' }
-        ]
-      },
-            {
-        text: 'OS',
-        items: [
-          { text: 'Linux', link: '/linux' },
-          { text: 'Windows', link: '/winsys' }
-        ]
-      }
-    ],
-    // sidebar: {
-    //   '/linux/': [ // Меню для раздела Linux
-    //     { text: 'Введение', items: [{ text: 'Установка', link: '/linux/install' }] }
-    //   ],
-    //   '/windows/': [ // Меню для раздела Windows
-    //     { text: 'Основы', items: [{ text: 'CMD', link: '/windows/cmd' }] }
-    //   ],
-    //   // Меню по умолчанию (если путь не совпал выше)
-    //   '/': [
-    //     { text: 'Главная', items: [{ text: 'О проекте', link: '/' }] }
-    //   ]
-    // },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+    // sidebar: [
+    //   {
+    //     text: 'Примеры',
+    //     items: [
+    //       { text: 'Примеры разметки', link: '/markdown-examples' },
+    //       { text: 'Примеры API', link: '/api-examples' }
+    //     ]
+    //   },
+    //         {
+    //     text: 'OS',
+    //     items: [
+    //       { text: 'Linux', link: '/linux' },
+    //       { text: 'Windows', link: '/winsys' }
+    //     ]
+    //   }
+    // ],
+
+    sidebar: {
+      "/linux": [createLinuxBlock("Linux"), createStandardBlock("Меню")],
+      "/windows": [createWindowsBlock("Windows"), createStandardBlock("Меню")],
+      // Меню по умолчанию (если путь не совпал выше)
+      "/": [
+        createStandardBlock("Меню"),
+        createLinuxBlock("Linux"),
+        createWindowsBlock("Windows"),
+        // { text: 'Главная', items: [{ text: 'Главная', link: '/' },{ text: 'Навигация', link: '/menu' }] },
+        // { text: "Linux", items: [{ text: "Linux", link: "/linux" }] },
+      ],
+    },
+
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ]
+  },
+});
